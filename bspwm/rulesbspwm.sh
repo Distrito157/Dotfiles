73c23cc2767c43fd2cb65f3744a11c58a380dc73
Felipi Soares de Oliveira <felipidst@gmail.com>
2023-10-31 00:18:05 -0300

    Flameeengo

--- bspwm/rulesbspwm.sh
+++ bspwm/rulesbspwm.sh
@@ -5,7 +5,7 @@
 #  NOTE: Estados = { tiled, floating, fullscreen }
 
 bspc rule -a Gimp desktop='^3' state=fullscreen follow=off
-bspc rule -a Chromium desktop='^6' state=fullscreen focus=on
+bspc rule -a Chromium desktop='^6' state=fullscreen follow=off focus=off
 bspc rule -a 'feh' state=floating
 
 # }}}
